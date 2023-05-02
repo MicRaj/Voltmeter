37bb911b5138cf9d2136a7d48208cb1df9e787e3
bg651 <126028423+bg651@users.noreply.github.com>
2023-05-02 21:33:38 +0100

    Update main.c

--- Voltmeter.X/main.c
+++ Voltmeter.X/main.c
@@ -4,69 +4,68 @@
 #pragma config WDTE = OFF
 #pragma config PWRTE = ON
 #pragma config CP = OFF
-
-#define _XTAL_FREQ  4000000 // clock speed#
-
+#define _XTAL_FREQ  4000000 // clock speed
 #include <xc.h> // Include ADC and LCD library
 #include "adc.h"
 #include "lcd.h"
 #include "string.h"
 // =====================================================
+
 // global variables
 // =====================================================
 unsigned short int adcVal = 0; // Storage for the raw ADC value
-unsigned volatile char toggleHold = 0b0;
-// =====================================================
-// isr
+unsigned volatile char toggleHold = 0b0; // global variable for toggle button
 // =====================================================
 
+// ISR
+// =====================================================
+void __interrupt() isr() {
+    INTCONbits.INTF = 0; // reset interrupt flag
+    toggleHold = ~toggleHold; // invert toggleHold variable
+    delay_100ms();
+    return;
+}
+// =====================================================
+
+// delay functions
+// =====================================================
 void delay_100ms() {
     __delay_ms(100);
     return;
 }
+// 100ms delay function - done to optimise storage on pic
 
-void delay_1000ms() {
+void delay_2000ms() {
     for (int i = 0; i < 20; i++) {
         delay_100ms();
     }
     return;
 }
-
-void __interrupt() isr() {
-    // Reset the interrupt flag
-    INTCONbits.INTF = 0;
-    // Toggle the state of LED2, ~ is the complement
-    toggleHold = ~toggleHold;
-    delay_100ms();
-    return;
-}
-
+// 2000ms delay function - just loops the delay 100ms 20 times
 // =====================================================
+
 // welcome function
 // =====================================================
-
 void welcomeMessage(void) {
-    //On power-up the LCD should display a welcome message to the user that includes the range of possible input voltages.
-    char msg[] = "Hi";
+    char msg[] = "Hi"; // welcome message
     Lcd_Clear();
     Lcd_Set_Cursor(1, 1);
     Lcd_Write_String(msg);
-    delay_1000ms();
+    delay_2000ms();
     Lcd_Clear();
     Lcd_Set_Cursor(1, 1);
-    strcpy(msg, "0-5V");
-
+    strcpy(msg, "0-5V"); // voltage range
     Lcd_Write_String(msg);
-    delay_1000ms();
+    delay_2000ms();
     Lcd_Clear();
 }
 // =====================================================
 
 // main
 // =====================================================
-
 void main(void) {
-
+    // main initialisation
+    // ----------------------------
     TRISB = 0b01000001; // RB7 INPUT
     TRISA = 0b01000;
     // Set CS high and CLK low for ADC
@@ -75,60 +74,68 @@ void main(void) {
     // Set the R/W LCD pin to Write
     RW = 0;
     // Init the LCD
+    // ----------------------------
 
+    //interrupt set
+    // ----------------------------
     // Interrupt on the rising edge
     OPTION_REGbits.INTEDG = 1;
     // Enable the external interrupt
     INTCONbits.INTE = 1;
     // Global interrupt enable
     INTCONbits.GIE = 1;
+    // ----------------------------
 
-    Lcd_Init();
-    welcomeMessage();
-    unsigned short int d1;
-    unsigned short int d2;
-    unsigned short int d3;
-    unsigned short int d4;
+    Lcd_Init(); // initialise the lcd
+    welcomeMessage(); // calls welcome message function
+    unsigned short int ADC1_Digit1;
+    unsigned short int ADC1_Digit2;
+    unsigned short int ADC2_Digit1;
+    unsigned short int ADC2_Digit2;
     while (1) {
         while (toggleHold) {
             Lcd_Set_Cursor(1, 1);
-            Lcd_Write_Int(d1);
+            Lcd_Write_Int(ADC1_Digit1);
             Lcd_Set_Cursor(1, 2);
             Lcd_Write_Char('.');
             Lcd_Set_Cursor(1, 3);
-            Lcd_Write_Int(d2);
+            Lcd_Write_Int(ADC1_Digit2);
             Lcd_Set_Cursor(1, 6);
-            Lcd_Write_Int(d3);
+            Lcd_Write_Int(ADC2_Digit1);
             Lcd_Set_Cursor(1, 7);
             Lcd_Write_Char('.');
             Lcd_Set_Cursor(1, 8);
-            Lcd_Write_Int(d4);
+            Lcd_Write_Int(ADC2_Digit2);
         }
-        // Get the current ADC output code as an integer
-        adcVal = readADC1();
-        d1 = adcVal / 204;
-        d2 = ((adcVal % 204)*10 / 204);
-        adcVal = readADC2();
-        d3 = adcVal / 204;
-        d4 = ((adcVal % 204)*10 / 204);
+        // reading ADCs
+        // ----------------------------
+        adcVal = readADC1(); // reads the 1st ADC
+        ADC1_Digit1 = adcVal / 204;
+        ADC1_Digit2 = ((adcVal % 204)*10 / 204);
+        adcVal = readADC2(); // reads 2nd ADC
+        ADC2_Digit1 = adcVal / 204;
+        ADC2_Digit2 = ((adcVal % 204)*10 / 204);
+        // turns the integer value into a 1 decimal place value to display on the lcd
+        // ----------------------------
 
+        // writing to LCD
+        // ----------------------------
         Lcd_Clear();
         Lcd_Set_Cursor(1, 1);
-        Lcd_Write_Int(d1);
+        Lcd_Write_Int(ADC1_Digit1);
         Lcd_Set_Cursor(1, 2);
         Lcd_Write_Char('.');
         Lcd_Set_Cursor(1, 3);
-        Lcd_Write_Int(d2);
+        Lcd_Write_Int(ADC1_Digit2);
         Lcd_Set_Cursor(1, 6);
-        Lcd_Write_Int(d3);
+        Lcd_Write_Int(ADC2_Digit1);
         Lcd_Set_Cursor(1, 7);
         Lcd_Write_Char('.');
         Lcd_Set_Cursor(1, 8);
-        Lcd_Write_Int(d4);
-
+        Lcd_Write_Int(ADC2_Digit2);
+        // ----------------------------
         delay_100ms();
     }
     return;
 }
 // =====================================================
-
